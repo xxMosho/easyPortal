b53c6127949e0b9416c204e9a19b0c3f02e1255e
xxMosho <markmagedamazon@gmail.com>
2023-05-26 20:23:16 +0300

    Testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Navbar from "@/Components/Navbar"
 export default function Home() {
   return (
     <>
-      <div className="bg-gradient-to-t from-white to-primary h-screen">
+      <div className="bg-gradient-to-tr from-base-100 to-primary h-screen">
         <center>
           <br></br>
           <Navbar />
